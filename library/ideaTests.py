35900f2b47dfebca8959e00711004bbcfee36edf
Nave Weksler <naveweksler@gmail.com>
2021-07-15 21:35:13 +0300

    We can use this I will build the event loop tommorow

---
+++ library/ideaTests.py
@@ -0,0 +1,18 @@
+
+def eventListener(str):
+    print(str)
+    # add to event loop here
+    return decorateEventListener
+
+def decorateEventListener(func):
+    def inner():
+        print("decorateEventListener")
+        func("TYPE EVENT")
+    return inner
+
+
+@eventListener("TYPE")
+def onTYPE(event):
+    print("Hello World", event)
+
+onTYPE()
